79b21b6a7a8771913f1f158ba8bba6bb405f11e1
benit58383 <benit58383@gmail.com>
2023-03-28 16:20:58 +0700

    fix routes

--- functions/src/api/routes.js
+++ functions/src/api/routes.js
@@ -2,6 +2,6 @@ let userRouter = require('./user/routes.js');
 
 module.exports = (app) => {
   app.get("/.netlify/functions/server", (req, res) => {res.json({ message: "Welcome to ben's application!" })});
-  app.use("/.netlify/functions/server", userRouter);
+  app.use("/.netlify/functions/server/api", userRouter);
 }
 
